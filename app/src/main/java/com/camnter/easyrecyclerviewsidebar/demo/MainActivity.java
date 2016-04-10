d992de50f0eaf2ac6c40cce66f4a97ca52b1e8bd
CaMnter <yuanyu.camnter@gmail.com>
2016-04-10 17:31:01 +0800

    Adaptive EasyImageSection

--- app/src/main/java/com/camnter/easyrecyclerviewsidebar/demo/MainActivity.java
+++ app/src/main/java/com/camnter/easyrecyclerviewsidebar/demo/MainActivity.java
@@ -4,6 +4,10 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 import com.camnter.easyrecyclerviewsidebar.EasyRecyclerViewSidebar;
+import com.camnter.easyrecyclerviewsidebar.sections.EasyImageSection;
+import com.camnter.easyrecyclerviewsidebar.sections.EasySection;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,7 +31,32 @@ public class MainActivity extends AppCompatActivity {
 
 
     private void initData() {
-        String[] sections = { "★", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L" };
-        this.mSidebar.setSections(sections);
+        String[] sections = { "A", "B", "C" };
+        List<EasySection> sections1 = new ArrayList<>();
+        EasyImageSection imageSection1 = new EasyImageSection(R.drawable.ic_camnter,
+                EasyImageSection.ROUND);
+        EasyImageSection imageSection2 = new EasyImageSection(R.drawable.ic_drakeet,
+                EasyImageSection.ROUND);
+        sections1.add(imageSection2);
+        sections1.add(imageSection1);
+
+        EasySection section1 = new EasySection("A");
+        EasySection section2 = new EasySection("B");
+        EasySection section3 = new EasySection("C");
+        EasySection section4 = new EasySection("D");
+        EasySection section5 = new EasySection("E");
+        sections1.add(section1);
+        sections1.add(section2);
+        sections1.add(section3);
+        sections1.add(section4);
+        sections1.add(section5);
+
+        EasyImageSection imageSection4 = new EasyImageSection(R.drawable.ic_camnter,
+                EasyImageSection.ROUND);
+        EasyImageSection imageSection5 = new EasyImageSection(R.drawable.ic_drakeet,
+                EasyImageSection.ROUND);
+        sections1.add(imageSection5);
+        sections1.add(imageSection4);
+        this.mSidebar.setSections(sections1);
     }
 }
--- easyrecyclerviewsidebar/src/main/java/com/camnter/easyrecyclerviewsidebar/EasyRecyclerViewSidebar.java
+++ easyrecyclerviewsidebar/src/main/java/com/camnter/easyrecyclerviewsidebar/EasyRecyclerViewSidebar.java
@@ -2,17 +2,29 @@ package com.camnter.easyrecyclerviewsidebar;
 
 import android.annotation.TargetApi;
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapShader;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Matrix;
 import android.graphics.Paint;
+import android.graphics.RectF;
+import android.graphics.Shader;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.v4.content.res.ResourcesCompat;
 import android.util.AttributeSet;
+import android.util.DisplayMetrics;
+import android.util.TypedValue;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.TextView;
 import com.camnter.easyrecyclerviewsidebar.sections.EasyImageSection;
 import com.camnter.easyrecyclerviewsidebar.sections.EasySection;
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Description：EasyRecyclerViewSidebar
@@ -21,21 +33,36 @@ import java.util.ArrayList;
  */
 public class EasyRecyclerViewSidebar extends View {
 
+    private static final String DEFAULT_ONE_LETTER_MEASURE_WIDTH_CASE = "M";
+
     private static final int DEFAULT_VIEW_BACKGROUND = 0x40000000;
     private static final int DEFAULT_FONT_COLOR = 0xff444444;
     private static final int DEFAULT_FONT_SIZE = 14;
 
     private static final int MAX_SECTION_COUNT = 30;
 
-    private Paint paint;
+    private static final int DEFAULT_IMAGE_SECTION_BORDER_RADIUS = 6;
+
+    private Paint letterPaint;
+    private Paint imagePaint;
+
     private TextView floatView;
     private float sectionHeight;
     private float allSectionHeight;
     private float sectionFontSize;
+    private float letterWidth = 0;
+    private float letterHeight = 0;
+
+    private Matrix imageSectionMatrix;
+    private float imageSectionRadius;
+    private RectF imageSectionRect;
+    protected float imageSectionBorderRadius;
 
     private int viewWidth;
     private int viewHeight;
     private int viewHalfWidth;
+
+    private DisplayMetrics metrics;
 
     private ArrayList<EasySection> sections;
 
@@ -68,13 +95,30 @@ public class EasyRecyclerViewSidebar extends View {
 
 
     private void init(Context context, AttributeSet attrs) {
-        this.sectionFontSize = this.sp2px(context, DEFAULT_FONT_SIZE);
-        this.paint = new Paint();
-        this.paint.setAntiAlias(true);
-        this.paint.setTextAlign(Paint.Align.CENTER);
-        this.paint.setColor(DEFAULT_FONT_COLOR);
-        this.paint.setTextSize(this.sectionFontSize);
+        this.metrics = this.getResources().getDisplayMetrics();
+        this.sectionFontSize = this.sp2px(DEFAULT_FONT_SIZE);
         this.sections = new ArrayList<>();
+        this.imageSectionMatrix = new Matrix();
+        this.initPaints();
+        float[] letterProperty = this.measureText(this.letterPaint,
+                DEFAULT_ONE_LETTER_MEASURE_WIDTH_CASE);
+        this.letterWidth = letterProperty[0];
+        this.letterHeight = letterProperty[1];
+        this.imageSectionRadius = this.letterWidth / 2;
+        this.imageSectionRect = new RectF(0, 0, this.letterWidth, this.letterWidth);
+        this.imageSectionBorderRadius = this.dp2px(DEFAULT_IMAGE_SECTION_BORDER_RADIUS);
+    }
+
+
+    private void initPaints() {
+        this.letterPaint = new Paint();
+        this.letterPaint.setAntiAlias(true);
+        this.letterPaint.setTextAlign(Paint.Align.CENTER);
+        this.letterPaint.setColor(DEFAULT_FONT_COLOR);
+        this.letterPaint.setTextSize(this.sectionFontSize);
+
+        this.imagePaint = new Paint();
+        this.imagePaint.setAntiAlias(true);
     }
 
 
@@ -84,6 +128,9 @@ public class EasyRecyclerViewSidebar extends View {
         this.viewHeight = this.getHeight();
         this.viewHalfWidth = this.viewWidth / 2;
         this.sectionHeight = this.viewHeight / MAX_SECTION_COUNT;
+        boolean isPreviousImage = false;
+        boolean isPreviousLetter = false;
+
         if (this.sections.size() > 0) {
             this.allSectionHeight = this.sectionHeight * this.sections.size();
             float top = this.viewHeight / 2 - allSectionHeight / 2 + this.sectionHeight / 2 -
@@ -91,10 +138,39 @@ public class EasyRecyclerViewSidebar extends View {
             for (int i = 0; i < this.sections.size(); i++) {
                 EasySection section = this.sections.get(i);
                 if (section instanceof EasyImageSection) {
-
+                    EasyImageSection imageSection = (EasyImageSection) section;
+                    this.setPaintShader(imageSection);
+                    canvas.save();
+                    if (isPreviousLetter) {
+                        top -= this.letterWidth - (Math.max(this.letterHeight, this.sectionHeight) -
+                                Math.min(this.letterHeight, this.sectionHeight));
+                    }
+                    canvas.translate(this.viewHalfWidth - this.letterWidth / 2,
+                            top + this.sectionHeight * i);
+                    switch (imageSection.imageType) {
+                        case EasyImageSection.ROUND: {
+                            canvas.drawRoundRect(this.imageSectionRect,
+                                    this.imageSectionBorderRadius, this.imageSectionBorderRadius,
+                                    this.imagePaint);
+                            break;
+                        }
+                        case EasyImageSection.CIRCLE: {
+                            canvas.drawCircle(this.viewHalfWidth, top + this.sectionHeight * i,
+                                    this.imageSectionRadius, this.imagePaint);
+                            break;
+                        }
+                    }
+                    canvas.restore();
+                    isPreviousImage = true;
+                    isPreviousLetter = false;
                 } else {
+                    if (isPreviousImage) {
+                        top += this.letterWidth;
+                    }
                     canvas.drawText(section.description, this.viewHalfWidth,
-                            top + this.sectionHeight * i, this.paint);
+                            top + this.sectionHeight * i, this.letterPaint);
+                    isPreviousImage = false;
+                    isPreviousLetter = true;
                 }
             }
         } else {
@@ -159,9 +235,70 @@ public class EasyRecyclerViewSidebar extends View {
     }
 
 
-    private int sp2px(Context context, float sp) {
-        float density = context.getResources().getDisplayMetrics().scaledDensity;
-        return (int) (sp * density + 0.5F);
+    private void setPaintShader(@NonNull EasyImageSection imageSection) {
+        Drawable drawable;
+        if (imageSection.drawable == null && imageSection.resId >= 0) {
+            drawable = ResourcesCompat.getDrawable(this.getResources(), imageSection.resId, null);
+        } else {
+            drawable = imageSection.drawable;
+        }
+        if (drawable == null) return;
+
+        Bitmap bitmap = this.drawableToBitmap(drawable);
+        BitmapShader sectionBitmapShader = new BitmapShader(bitmap, Shader.TileMode.CLAMP,
+                Shader.TileMode.CLAMP);
+        float scale = 1.0f;
+        switch (imageSection.imageType) {
+            case EasyImageSection.ROUND: {
+                scale = Math.max(this.letterWidth * 1.0f / bitmap.getWidth(),
+                        this.letterWidth * 1.0f / bitmap.getHeight());
+                break;
+            }
+            case EasyImageSection.CIRCLE: {
+                scale = this.letterWidth * 1.0f / Math.min(bitmap.getWidth(), bitmap.getHeight());
+                break;
+            }
+        }
+        this.imageSectionMatrix.setScale(scale, scale);
+        sectionBitmapShader.setLocalMatrix(this.imageSectionMatrix);
+        this.imagePaint.setShader(sectionBitmapShader);
+    }
+
+
+    private Bitmap drawableToBitmap(Drawable drawable) {
+        if (drawable instanceof BitmapDrawable) ((BitmapDrawable) drawable).getBitmap();
+
+        int width = drawable.getIntrinsicWidth();
+        int height = drawable.getIntrinsicHeight();
+        Bitmap bitmap = this.createBitmapSafely(width, height, Bitmap.Config.ARGB_8888, 1);
+        Canvas canvas = new Canvas(bitmap);
+        drawable.setBounds(0, 0, width, height);
+        drawable.draw(canvas);
+        return bitmap;
+    }
+
+
+    public Bitmap createBitmapSafely(int width, int height, Bitmap.Config config, int retryCount) {
+        try {
+            return Bitmap.createBitmap(width, height, config);
+        } catch (OutOfMemoryError e) {
+            e.printStackTrace();
+            if (retryCount > 0) {
+                System.gc();
+                return createBitmapSafely(width, height, config, retryCount - 1);
+            }
+            return null;
+        }
+    }
+
+
+    public float dp2px(int dp) {
+        return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, this.metrics);
+    }
+
+
+    private float sp2px(float sp) {
+        return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, sp, this.metrics);
     }
 
 
@@ -170,6 +307,28 @@ public class EasyRecyclerViewSidebar extends View {
         for (String section : sections) {
             this.sections.add(new EasySection(section));
         }
+        this.invalidate();
+    }
+
+
+    public void setSections(List<EasySection> sections) {
+        this.sections.clear();
+        this.sections.addAll(sections);
+        this.invalidate();
+    }
+
+
+    /**
+     * @param paint paint
+     * @param text text
+     * @return float[] [0]=width [1]=height
+     */
+    private float[] measureText(Paint paint, String text) {
+        float[] property = new float[2];
+        property[0] = paint.measureText(text);
+        Paint.FontMetrics fontMetrics = paint.getFontMetrics();
+        property[1] = fontMetrics.descent - fontMetrics.ascent + fontMetrics.leading;
+        return property;
     }
 
 
